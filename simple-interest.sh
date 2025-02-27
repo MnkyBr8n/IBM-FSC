49e0655bdd79fbe8f45f128f2c46f9b5ffebbc33
MnkyBr8n <yxyel315@outlook.com>
2025-02-26 23:15:18 -0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # MnkyBr8n
 
  # Input:
    # p, principal amount
